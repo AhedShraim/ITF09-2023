7fc68bf735332e1fbbc691939a7b7132fe6a8164
unknown <ahedshraim96@gmail.com>
2023-09-17 03:09:41 +0300

    Initial commit

---
+++ Task 1.py
@@ -0,0 +1,23 @@
+def add(x,y):
+    total = x + y
+    return total
+def sub(x,y):
+    total = x - y
+    return total
+def div(x,y):
+    total = x / y
+    return total
+def mul(x,y):
+    total = x * y
+    return total
+
+num1 = int(input("Enter Number 1 "))
+num2 = int(input("Enter Number 2 "))
+total = add(num1,num2)
+print("Total of the addition process is ",total)
+total = sub(num1,num2)
+print("Total of the subtraction process is ",total)
+total = div(num1,num2)
+print("Total of the division process is ",total)
+total = mul(num1,num2)
+print("Total of the multiplication process is ",total)
